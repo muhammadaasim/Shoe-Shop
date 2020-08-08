59210a06269f8c4af2f95fc7c3542e875426519b
muhammadaasim <47273318+muhammadaasim@users.noreply.github.com>
2020-08-08 15:19:33 +0500

    Update RouteConfig.js

--- src/RouteConfig.js
+++ src/RouteConfig.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router,Routes,Link,Route} from 'react-router-dom';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 import Home from './Components/Home';
 import About from './Components/About';
 import Products from './Components/Products';
